71a621a5f8a206393c444348b282ec65ed3a3933
jeswi2 <jeswimathewm@gmail.com>
2022-10-20 01:45:11 +0530

    ma

---
+++ src/main/java/com/nestdigital/xyzbackend/XyzBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.xyzbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyzBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyzBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestdigital/xyzbackend/XyzBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.xyzbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XyzBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
